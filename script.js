dcc94a07e00e957bb8ccfa0d25fda9632926f883
pkklipe <felipe.iastrenski@escola.pr.gov.br>
2026-08-11 09:55:35 -0300

    okko

---
+++ script.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelecrtorAll("button");
+
+botoes.forEach(botao => {
+    botao,addEventventilistener("click", )
+
+    function curtir() {
+        let curtida = document.querySelecrtor("span")
+        curtida.textContent++
+    }
+});
